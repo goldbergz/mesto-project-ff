1cddfd79576b2fabf64e0e52246cdddade6b7d66
goldbergz <zolotogornaya22@mail.ru>
2025-09-26 15:58:07 +0300

    chore: setup webpack and display images

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import './pages/index.css'
+import { initialCards } from './cards.js';
+
 // Темплейт карточки
 const cardTemplate = document.querySelector("#card-template").content.querySelector(".places__item");
 

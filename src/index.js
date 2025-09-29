cd6c2e572dd2ffbe3c6969524d164ff148b98fec
goldbergz <zolotogornaya22@mail.ru>
2025-09-29 11:24:41 +0300

    feat: editing profile data

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 import './pages/index.css'
 import { initialCards } from './scripts/cards.js';
 import { createCardElement, handleDeleteCard } from './scripts/components/card.js';
-import { openModal, closeModal, closeModalOverlay } from './scripts/components/modal.js';
+import { openModal, closeModal, closeModalOverlay, formEditProfileElement, nameInput, jobInput } from './scripts/components/modal.js';
+
+export const profileTitle = document.querySelector('.profile__title');
+export const profileDescription = document.querySelector('.profile__description');
 
 const placesWrap = document.querySelector(".places__list");
 const profileEditButton = document.querySelector('.profile__edit-button');
 const addNewCardButton = document.querySelector('.profile__add-button');
-const cardImage = document.querySelector('.places__item');
 
 const modalWindows = document.querySelectorAll('.popup');
 const modalnewCardWindow = document.querySelector('.popup_type_new-card');
-const modalEditProfiledWindow = document.querySelector('.popup_type_edit');
+export const modalEditProfiledWindow = document.querySelector('.popup_type_edit');
 export const modalImageWindow = document.querySelector('.popup_type_image');
 
 const closeModalButtons = document.querySelectorAll('.popup__close');
@@ -27,7 +29,12 @@ addNewCardButton.addEventListener('click', () => openModal(modalnewCardWindow));
 closeModalButton2.addEventListener('click', () => closeModal(modalnewCardWindow));
 document.addEventListener('click', (event) => closeModalOverlay(event, modalWindows));
 
-profileEditButton.addEventListener('click', () => openModal(modalEditProfiledWindow));
+profileEditButton.addEventListener('click', () => {
+  nameInput.value = profileTitle.textContent;
+  jobInput.value = profileDescription.textContent;
+  openModal(modalEditProfiledWindow)
+});
+
 closeModalButton1.addEventListener('click', () => closeModal(modalEditProfiledWindow));
 
 closeModalButton3.addEventListener('click', () => closeModal(modalImageWindow));
--- src/scripts/components/modal.js
+++ src/scripts/components/modal.js
@@ -1,3 +1,9 @@
+import { profileTitle, profileDescription, modalEditProfiledWindow } from '../../index.js';
+
+export const formEditProfileElement = document.forms['edit-profile'];
+export const nameInput = formEditProfileElement.elements.name;
+export const jobInput = formEditProfileElement.elements.description;
+
 function onEsc(event) {
   if (event.key === 'Escape') {
     const modal = document.querySelector('.popup_is-opened');
@@ -24,3 +30,14 @@ export function closeModalOverlay(event, modals) {
     }
   }
 }
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  
+  profileTitle.textContent = nameInput.value;
+  profileDescription.textContent = jobInput.value;
+
+  closeModal(modalEditProfiledWindow);
+}
+
+formEditProfileElement.addEventListener('submit', handleFormSubmit);
